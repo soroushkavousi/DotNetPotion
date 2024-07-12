e4a1c53a4ada23809fce86a73ce384fd7029d633
soroushkavousi <soroushkavousi.me@gmail.com>
2024-07-12 19:50:14 +0330

    style: change the ScopedTaskRunner location

--- DotNetPotion/ScopedTaskRunner/IScopedTaskRunner.cs
+++ DotNetPotion/ScopedTaskRunner/IScopedTaskRunner.cs
@@ -3,7 +3,7 @@ using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Threading.Tasks;
 
-namespace DotNetPotion.Services.ScopedTaskRunner
+namespace DotNetPotion.ScopedTaskRunner
 {
     public interface IScopedTaskRunner
     {
